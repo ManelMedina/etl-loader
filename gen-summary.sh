cf9f0d61140074d0e0edf710c5fca1a1d36286fd
Aaron Kaplan <aaron@lo-res.org>
2016-05-27 19:50:52 +0200

    modify and make more generic for all kinds of risks

--- gen-summary.sh
+++ gen-summary.sh
@@ -1,21 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# DNS
+# Risk IDs:
+# id |   name
+#----+----------
+#  1 | opendns
+#  2 | openntp
+#  3 | spam
+#  4 | opensnmp
+#  5 | openssdp
 
-# by country
-# opendns-place-annual.csv
-echo "select  extract(year from date_trunc('year', ts)) as year, \"place.cc\" as country, count(*) as value  from hits group by year,country order by value desc;" | \
-	psql -t -F"," -A iphistory > opendns-place-annual.csv
+declare -A risks=( ["1"]="opendns" ["2"]="openntp" ["3"]="opensnmp" ["4"]="openssdp" )
 
-# opendns-place-monthly.csv
-echo "select  date_trunc('month', ts) as date, \"place.cc\" as country, count(*) as value  from hits group by date,country order by value desc;" | \
-	psql -t -F"," -A iphistory > opendns-place-monthly.csv
+for rid in ${!risks[@]}; do 
+	val=${risks[$rid]}
 
-# by ASN
-# opendns-place-annual.csv
-echo "select  extract(year from date_trunc('year', ts)) as year, asn as asn, count(*) as value  from hits group by year,asn order by value desc;" | \
-	psql -t -F"," -A iphistory > opendns-asn-annual.csv
+	# by country, annual
+	outfile="$val-place-annual.csv"
+	echo "select  extract(year from date_trunc('year', ts)) as year, \"place.cc\" as country, count(*) as value  from hits where risk_id=$rid group by year,country order by value desc;" | \
+		psql -t -F"," -A iphistory > $outfile
 
-# opendns-place-monthly.csv
-echo "select  date_trunc('month', ts) as date, asn as asn, count(*) as value  from hits group by date,asn order by value desc;" | \
-	psql -t -F"," -A iphistory > opendns-asn-monthly.csv
+	# by country, monthly
+	outfile="$val-place-monthly.csv"
+	echo "select  date_trunc('month', ts) as date, \"place.cc\" as country, count(*) as value  from hits where risk_id=$rid group by date,country order by value desc;" | \
+		psql -t -F"," -A iphistory > $outfile
+
+	# by ASN, annual
+	outfile="$val-asn-annual.csv"
+	echo "select  extract(year from date_trunc('year', ts)) as year, asn as asn, count(*) as value  from hits where risk_id=$rid group by year,asn order by value desc;" | \
+		psql -t -F"," -A iphistory > $outfile
+
+	# by ASN, monthly
+	outfile="$val-asn-monthly.csv"
+	echo "select  date_trunc('month', ts) as date, asn as asn, count(*) as value  from hits where risk_id=$rid group by date,asn order by value desc;" | \
+		psql -t -F"," -A iphistory > $outfile
+done
+
+# Overall file with all risks in one
+outfile="all-place-annual.csv"
+echo "select risk.name as risk_name, extract(year from date_trunc('year', ts)) as year, \"place.cc\" as country, count(*) as value  from risk,hits where risk_id=risk.id group by risk_name,year,country order by value desc;" | \
+	psql -t -F"," -A iphistory > $outfile
+
