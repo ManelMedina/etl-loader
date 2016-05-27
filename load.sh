676da1324a8bb0e88e6ad6d5c53455e7f1be5f3e
Aaron Kaplan <aaron@lo-res.org>
2016-05-27 21:23:49 +0200

    remove gps postition

--- load.sh
+++ load.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 table=$1
-psql iphistory -c "copy $table (ts,risk_id,ip,asn,\"place.cc\",\"place.lat\",\"place.lon\") from  STDIN delimiters ',' csv quote '''';" 
+#psql iphistory -c "copy $table (ts,risk_id,ip,asn,\"place.cc\",\"place.lat\",\"place.lon\") from  STDIN delimiters ',' csv quote '''';" 
+psql iphistory -c "copy $table (ts,risk_id,ip,asn,\"place.cc\") from  STDIN delimiters ',' csv quote '''';" 
 
 
