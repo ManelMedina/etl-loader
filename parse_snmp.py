d4616682fbbb43cc5f05c374644fd00ecdeced4a
Aaron Kaplan <aaron@lo-res.org>
2016-06-01 02:18:55 +0200

    intermediate version of snmp. Does not really work yet

--- parse_snmp.py
+++ parse_snmp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 import sys
@@ -38,23 +38,28 @@ sep=','
 # timestamp (epoche) UTC : ip addr : second IP : snmp string/snmp data
 
 
+i=0
 count_unparseable=0
 count_parseable=0
-reader = csv.reader(sys.stdin, delimiter=':', quotechar="'")
-for line in reader:
-    ts=float(line[0])
-    # postgresql format: 2016-05-08 02:00:04.126444+02
-    ts = time.strftime("%Y-%m-%d %H:%M:%S.0+00", time.localtime(ts))     # remember, we are UTC 
-    ip = line[1]
-    try:
-        print("'{ts}'{sep}{risk}{sep}'{ip}'".format(sep=sep,risk=4,ts=ts,ip=ip))
-        count_parseable+=1
-    except Exception as e:
-        print(type(e), file=sys.stderr)    # the exception instance
-        print(e.args, file=sys.stderr)     # arguments stored in .args
-        print(e, file=sys.stderr) 
-        print("could not parse '{}'".format(line), file=sys.stderr) 
-        count_unparseable+=1
+reader = csv.reader(sys.stdin, delimiter=':', quotechar="'") 
+try:
+    for line in reader:
+        ts=float(line[0])
+        # postgresql format: 2016-05-08 02:00:04.126444+02
+        ts = time.strftime("%Y-%m-%d %H:%M:%S.0+00", time.localtime(ts))     # remember, we are UTC 
+        ip = line[1]
+        try:
+            print("'{ts}'{sep}{risk}{sep}'{ip}'".format(sep=sep,risk=4,ts=ts,ip=ip))
+            count_parseable+=1
+        except Exception as e:
+            print(type(e), file=sys.stderr)    # the exception instance
+            print(e.args, file=sys.stderr)     # arguments stored in .args
+            print(e, file=sys.stderr) 
+            print("line {}: could not parse '{}'".format(i,line), file=sys.stderr) 
+            count_unparseable+=1
+        i+=1
+except Exception as e:
+    print("line {}: could not parse '{}'".format(i,line), file=sys.stderr) 
 
 
 print("parseable lines: {}".format(count_parseable), file=sys.stderr)
